c6a645c0a56b04e84f06617ab7a427dbe1a738ad
karimsaabir9 <karimsapir9@gmail.com>
2025-12-16 22:09:56 +0300

    Added footer js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-  document.getElementById('menu-btn').addEventListener('click', toggleMenu);
-    function toggleMenu() {
-      const menu = document.getElementById('mobile-menu');
-      const icon = document.querySelector('#menu-btn i');
+document.getElementById("menu-btn").addEventListener("click", toggleMenu);
+document.getElementById("year").innerHTML = new Date().getFullYear();
+function toggleMenu() {
+  const menu = document.getElementById("mobile-menu");
+  const icon = document.querySelector("#menu-btn i");
 
-      menu.classList.toggle('hidden');
-      menu.classList.toggle('flex');
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("flex");
 
-      if (menu.classList.contains('hidden')) {
-        icon.classList.remove('fa-xmark');
-        icon.classList.add('fa-bars');
-      } else {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-xmark');
-      }
-    }
+  if (menu.classList.contains("hidden")) {
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  } else {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+  }
+}
